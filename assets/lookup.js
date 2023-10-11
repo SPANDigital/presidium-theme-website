fa2a4016e623669c6f325b6f2543d4db706434d2
meyerhp <helgard.meyer@spandigital.com>
2023-10-11 18:36:59 +0200

    Add automatic matching

--- assets/lookup.js
+++ assets/lookup.js
@@ -5,7 +5,10 @@ document.addEventListener('DOMContentLoaded', function() {
         let content = textNode.nodeValue;
         if(content) {
             for(let employee of data.employees) {
-                content = content?.replace(employee.official_email, `<span class="popup" data-ref="${employee.official_email}" style="color: red">${employee.official_email}</span>`);
+                content = content?.replace(employee.official_email, `<span class="popup" id="${employee.employee_id}" style="color: red">${employee.official_email}</span>`);
+                tippy(`#${employee.employee_id}`, {
+                    content: `${ employee.first_name } ${ employee.middle_name } ${ employee.last_name }, a member of the SPAN team with the employee ID ${ employee.employee_id }. Currently, ${ employee.first_name } holds the position of ${ employee.designation } and is employed with us on a ${ employee.employee_type } basis. If you wish to reach out, you can contact ${ employee.first_name } at their official Span email: ${ employee.official_email } or their personal email: ${ employee.personal_email }. ${ employee.first_name } began their journey with Span on ${ employee.joining_date } and has been contributing to the ${ employee.territory } territory. All of ${ employee.first_name }'s efforts are overseen by ${ employee.reporting_to }.`,
+                  });
             }
             const span = document.createElement('span');
             span.innerHTML = content;
@@ -16,19 +19,41 @@ document.addEventListener('DOMContentLoaded', function() {
         }
     }
 
+    function onHover(event) {
+        const email = event.target.getAttribute('data-ref');
+        console.log(email);
+    }
+
     function addClickListenerToSpans(element) {
         const popupSpans = element.querySelectorAll('span.popup');
         popupSpans.forEach(popupSpan => {
-            popupSpan.onclick = function(event) {
-                event.preventDefault();
-            };
+            popupSpan.addEventListener("mouseover", onHover);
         });
+    }
+
+    function showPopup(targetSpan) {
+        // Create the popup element
+        const popup = document.createElement('div');
+        popup.textContent = "Your popup content here"; // Modify this as needed
+        popup.style.position = 'absolute';
+        popup.style.background = '#fff';
+        popup.style.border = '1px solid #000';
+        popup.style.padding = '5px';
+        popup.style.zIndex = '1000';
+        popup.style.top = (targetSpan.getBoundingClientRect().top + window.scrollY) + 'px';
+        popup.style.left = (targetSpan.getBoundingClientRect().left + window.scrollX) + 'px';
+    
+        // Add a class or id to identify and remove it later
+        popup.classList.add('email-popup');
+    
+        // Append to the body
+        document.body.appendChild(popup);
     }
     
     function removeClickListenerFromSpans(element) {
         const popupSpans = element.querySelectorAll('span.popup');
         popupSpans.forEach(popupSpan => {
-            popupSpan.onclick = null;
+            popupSpan.removeEventListener("mousedown", onHover, true);
         });
     }
 
@@ -42,11 +67,20 @@ document.addEventListener('DOMContentLoaded', function() {
         }
     }
 
+    function containsRef(target) {
+        const content = target.textContent;
+        const options = data.employees.map(x => x.official_email).join('|');
+        const regex = new RegExp(options, 'i');
+        return regex.test(content);
+    }
+
     function handleIntersection(entries, observer) {
         entries.forEach(entry => {
             if (entry.isIntersecting) {
-                traverseAndReplace(entry.target);
-                addClickListenerToSpans(entry.target);
+                if(containsRef(entry.target)) {
+                    traverseAndReplace(entry.target);
+                    addClickListenerToSpans(entry.target);
+                }
             } else {
                 removeClickListenerFromSpans(entry.target);
             }
